051f52c36ce2e158fa151f9ffb0573489f4ce9ff
erza-503 <rendymahardika2007@gmail.com>
2024-12-08 21:07:11 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter  as Router } from 'react-router-dom'
 import App from './App'
 
 
